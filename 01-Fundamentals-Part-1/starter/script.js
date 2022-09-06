0bf09baab566b8c182e3e2efa4b8c595363a1d55
m0000g <m0000g@gmail.com>
2022-09-06 15:41:19 -0600

    changes during the lesson

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,20 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+
+let years = 3;
+let $function = 27;
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
