097914313488e761692952b6dd22eab1fabd1709
m0000g <m0000g@gmail.com>
2022-09-08 09:57:03 -0600

    Truthy and falsy values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -141,7 +141,7 @@ lines`); */
 
 // Taking Decisions: if / else Statements
 
-const age = 15;
+/* const age = 15;
 const isOldEnough = age >= 18;
 
 if (age >= 18) {
@@ -161,4 +161,56 @@ else {
     century = 21;
 }
 
-console.log(century);
+console.log(century); */
+
+// TYPE CONVERSION
+
+/* const inputYear = '1991';
+console.log(inputYear + 18);
+console.log(Number(inputYear) + 18);
+
+console.log(Number(`Jonas`)); // NaN Not - a valid Number
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+//  TYPE COERCION - Two values that have different types
+// Behind the scenes JS convert one of the values so the operation can be executed.
+
+console.log(`I'm ` + 23 + ` years old.`); // I'm 23 years old.
+console.log(`23` - `10` - 3); // 10
+console.log(`23` + `10` + 3); // 23103
+
+console.log(`40` / `2`); // 20
+console.log(`23` * `2`); // 46
+
+let n = `1` + 1; // 11
+n = n - 1; // 10
+console.log(n);
+ */
+
+// TRUTHY AND FALSY VALUES
+// 5 falsy values: 0, '', undefined, null, NaN -> becoome false after trying to convert them to boolean
+// Anything else is truthy values
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean(`Jonas`));
+console.log(Boolean({})); //empty
+console.log(Boolean(``)); //empty
+
+const money = 0;
+if (money) {
+    console.log(`Don't spend it all!`);
+} else {
+    console.log(`You should get a job!`);
+}
+
+let height;
+if (height) {
+    console.log(`Yey! Height is defined`);
+
+}
+else {
+    console.log(`Heigh is UNDEFINED`);
+}
