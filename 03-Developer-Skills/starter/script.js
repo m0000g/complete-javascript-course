55f165734544100152a8458917abe201d9e77b84
m0000g <m0000g@gmail.com>
2022-09-29 14:01:24 -0600

    prettier file

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,14 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = 23;
+
+const calcAge = birthyear => 2037 - birthyear;
+
+console.log(x);
+
+console.log(`Testing whatever`);
+
+console.log(calcAge(1991));
+
+// TODO highlighted.
