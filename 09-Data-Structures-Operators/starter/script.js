8a7054aad0e565921bde3606add22215261ab9a3
MG <m0000g@gmail.com>
2023-03-14 15:43:09 -0600

    Basic Maps

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,33 +1,13 @@
 'use strict';
 
-// Sets
-// A collection of unique values can't hae any duplicates
-const orderSet = new Set([
-  'Pasta',
-  'Pizza',
-  'Pizza',
-  'Risotto',
-  'Pasta',
-  'Pizza',
-]);
-
-console.log(orderSet);
-console.log(orderSet.size);
-console.log(orderSet.has('Pizza')); //true
-orderSet.add('Garlic Bread');
-orderSet.add('Garlic Bread');
-console.log(orderSet);
-orderSet.delete('Risotto');
-console.log(orderSet);
-
 // Data needed for a later exercise
-/* const flights =
+const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']; */
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
 // ENHANCED OBJECT LITERALS sample -> Compute name
-/* const openingHours = {
+const openingHours = {
   [weekdays[3]]: {
     open: 12,
     close: 22,
@@ -41,17 +21,17 @@ const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']; */
     close: 24,
   },
 };
- */
+
 // Data needed for first part of the section
-/* const restaurant = {
+const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'], */
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-// ES6 enhanced object literals
-/* openingHours,
+  // ES6 enhanced object literals
+  openingHours,
 
   order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
@@ -70,7 +50,65 @@ const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']; */
     console.log(mainIngridient);
     console.log(otherIngridient);
   },
-}; */
+};
+
+// Maps
+const rest = new Map();
+rest.set('name', 'Clasico Italiano');
+rest.set(1, 'Fiorenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are closed');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+//rest.clear(); // Removes all elements from map.
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+console.log(rest.get(arr));
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+// Sets
+// A collection of unique values can't hae any duplicates
+/* const orderSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+]);
+
+console.log(orderSet);
+console.log(orderSet.size);
+console.log(orderSet.has('Pizza')); //true
+orderSet.add('Garlic Bread');
+orderSet.add('Garlic Bread');
+console.log(orderSet);
+orderSet.delete('Risotto');
+console.log(orderSet);
+for (const order of orderSet) console.log(order); */
+
+//orderSet.clear(); // Clear all elements in the Set
+// Sets are commonly used to remove duplicated values from an array.
+// Sample
+/* const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffNew = [...new Set(staff)];
+console.log(staffNew); */
 
 // LOOPING OBJECTS - OBJECTS KEYS, VALUES AND ENTRIES
 
