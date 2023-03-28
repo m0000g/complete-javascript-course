29f2dc182108de1c54817e6073f1f276083451d6
MG <m0000g@gmail.com>
2023-03-28 13:43:40 -0600

    Example primitive values vs reference values inside a function

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,6 +1,38 @@
 'use strict';
 
-const bookings = [];
+const flight = 'LH234';
+const jonas = {
+  name: 'Maria Perez',
+  passport: 123123123343412,
+};
+
+const checkIn = function (flightNumber, passanger) {
+  flightNumber = 'LH999';
+  passanger.name = 'Mrs. ' + passanger.name;
+  if (passanger.passport === 123123123343412) {
+    console.log('Checkin');
+  } else {
+    console.log('Wrong passwport');
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+console.log(jonas);
+
+// Functions - Passing arguments value vs reference.
+
+// Functions - Default parameter
+
+/* const bookings = [];
 
 const createBooking = function (
   flightName,
@@ -8,8 +40,8 @@ const createBooking = function (
   price = 199 * numPassangers
 ) {
   // Old way to default
-  /* numPassangers = numPassangers || 1;
-  price = price || 199; */
+  //numPassangers = numPassangers || 1;
+  //price = price || 199;
 
   const booking = {
     flightName,
@@ -24,3 +56,4 @@ createBooking('LH123');
 createBooking('LH123', 2);
 createBooking('LH123', 4);
 createBooking('Lh123', undefined, 1000);
+ */
