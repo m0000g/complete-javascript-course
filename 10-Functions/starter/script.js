73f0d36501031a83e9a6ed7f24739857e683f7b2
MG <m0000g@gmail.com>
2023-03-28 11:24:38 -0600

    Starging with functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (
+  flightName,
+  numPassangers = 1,
+  price = 199 * numPassangers
+) {
+  // Old way to default
+  /* numPassangers = numPassangers || 1;
+  price = price || 199; */
+
+  const booking = {
+    flightName,
+    numPassangers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2);
+createBooking('LH123', 4);
+createBooking('Lh123', undefined, 1000);
