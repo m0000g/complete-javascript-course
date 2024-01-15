004b92a8a5387b8ff8a465b1d94f0672e430bf96
m0000g <m0000g@gmail.com>
2024-01-15 17:49:18 -0600

    Working on 11 array bankist starter file

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,11 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+arr.slice(2);
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4)); // Really 2 and 3
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
