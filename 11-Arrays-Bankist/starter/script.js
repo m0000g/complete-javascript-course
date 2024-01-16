4b0ea892c34f958b8930f8afa2d6038ea11857e1
m0000g <m0000g@gmail.com>
2024-01-15 18:06:53 -0600

    Working with Array methods: slice, splice, reverse, concat

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -75,6 +75,8 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
+// SLICE = Do not change the array
+
 let arr = ['a', 'b', 'c', 'd', 'e'];
 arr.slice(2);
 console.log(arr.slice(2));
@@ -82,3 +84,27 @@ console.log(arr.slice(2, 4)); // Really 2 and 3
 console.log(arr.slice(-2));
 console.log(arr.slice(-1));
 console.log(arr.slice(1, -2));
+console.log(arr.slice()); // To assign an array, the whole array.
+console.log([...arr]); // Also by using the spread operator. To create a shallow copy.
+
+// SPLICE = Mutates the array
+
+//console.log(arr.splice(2)); // Use to remove elements from an array
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+// REVERSE
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// CONCAT: Concatenate two arrays
+let letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]); // Same with the spread operator
+
+// JOIN
+console.log(letters.join('-')); // Join all the strings in an array with that separator
