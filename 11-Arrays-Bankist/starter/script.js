34c9dda32f9f402a60e977e847166adf3df3de4f
Gaby Perez <m0000g@gmail.com>
2024-01-18 17:53:35 -0600

    Code and notes from section 11

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -65,17 +65,18 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
 /////////////////////////////////////////////////
 
-// SLICE = Do not change the array
+// Array Methods
+/* // SLICE = Do not change the array
 
 let arr = ['a', 'b', 'c', 'd', 'e'];
 arr.slice(2);
@@ -86,6 +87,7 @@ console.log(arr.slice(-1));
 console.log(arr.slice(1, -2));
 console.log(arr.slice()); // To assign an array, the whole array.
 console.log([...arr]); // Also by using the spread operator. To create a shallow copy.
+
 
 // SPLICE = Mutates the array
 
@@ -108,3 +110,62 @@ console.log([...arr, ...arr2]); // Same with the spread operator
 
 // JOIN
 console.log(letters.join('-')); // Join all the strings in an array with that separator
+ */
+
+// AT
+
+/* const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// Getting the last element.
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+console.log(arr.at(-2));
+
+// Also works with strings
+console.log('jonas'.at(0));
+console.log('jonas'.at(-1)); */
+
+/* const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1} You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1} You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('---- FOREACH ----');
+// The order always should be current element, current index and the entired array
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1} You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1} You withdrew ${Math.abs(mov)}`);
+  }
+});
+ */
+
+// forEach with maps
+/* const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+// Set
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${_}: ${value}`);
+});
+ */
